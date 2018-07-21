4b68e60305167dde2b90d3464ca51ccab2b0c369
sunsetxh <wyc984872769@hotmail.com>
2018-07-21 17:06:05 +0800

     增加每个类的统计

--- getresofcls.py
+++ getresofcls.py
@@ -30,14 +30,16 @@ def readstarfile(filename):
 
 headdict, data = readstarfile(sys.argv[1])
 count = 0
-classcnt = [0, 0, 0, 0, 0]
+clscnt = [0, 0, 0, 0, 0]
+classrtcnt = [0, 0, 0, 0, 0]
 
 for item in data:
     items = item.split()
+    clscnt[int(items[headdict.get('ClassNumber')])] += 1
     if items[headdict.get('ImageName')].find('group{}'.format(items[headdict.get('ClassNumber')])) != -1:
         count += 1
-        classcnt[int(items[headdict.get('ClassNumber')]) - 1] += 1
+        classrtcnt[int(items[headdict.get('ClassNumber')]) - 1] += 1
 
 print('正确率 ＝ {:4f}％'.format(count * 100.0 / len(data)))
-for i in range(0,5):
-    print('class {} = {:4f}%'.format(i+1,classcnt[i] * 100.0 / 10000))
+for i in range(0, 5):
+    print('class {} = {:4f}%'.format(i + 1, classrtcnt[i] * 100.0 / clscnt[i]))
