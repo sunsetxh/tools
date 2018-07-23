eb7b7c4eb862d8871f3aa2fa13523829af6480ad
unknown <wyc984872769@hotmail.com>
2018-07-23 16:51:10 +0800

    增加每个类中不同组的统计

--- getresofcls.py
+++ getresofcls.py
@@ -32,6 +32,22 @@ headdict, data = readstarfile(sys.argv[1])
 count = 0
 clscnt = [0, 0, 0, 0, 0]
 classrtcnt = [0, 0, 0, 0, 0]
+cnt = [[0] * 5 for i in range(5)]
+
+for line in data:
+    vars = line.split()
+    clsnum = int(vars[headdict['ClassNumber']])
+    groupnum = int(vars[headdict['ImageName']][-6])
+    cnt[clsnum - 1][groupnum - 1] += 1
+
+print(cnt)
+
+f_det = open('detailedresult.txt', 'w')
+for i in range(5):
+    for j in range(5):
+        f_det.write('{:5d}\t'.format(cnt[i][j]))
+    f_det.write('\n')
+f_det.close()
 
 for item in data:
     items = item.split()
@@ -43,28 +59,28 @@ for item in data:
 print('正确率 ＝ {:4f}％'.format(count * 100.0 / len(data)))
 for i in range(0, 5):
     if clscnt[i] == 0:
-        print('class {} = {:4f}% number = {}'.format(i + 1, 0.0,clscnt[i]))
+        print('class {} = {:4f}% number = {}'.format(i + 1, 0.0, clscnt[i]))
     else:
-        print('class {} = {:4f}% number = {}'.format(i + 1, classrtcnt[i] * 100.0 / clscnt[i],clscnt[i]))
+        print('class {} = {:4f}% number = {}'.format(i + 1, classrtcnt[i] * 100.0 / clscnt[i], clscnt[i]))
 
-f=open('result.txt','a')
+f = open('result.txt', 'a')
 f.write('{}\n'.format(sys.argv[1]))
 f.write('{:20s}\t'.format('result'))
-for i in range(1,6):
-    f.write('{:>18s} {:1d}\t'.format('class',i))
+for i in range(1, 6):
+    f.write('{:>18s} {:1d}\t'.format('class', i))
 f.write('{:>20s}\t'.format('total'))
 f.write('\n')
 f.write('{:20s}\t'.format('Number of particles'))
-for i in range(0,5):
+for i in range(0, 5):
     f.write('{:20d}\t'.format(clscnt[i]))
 f.write('{:20d}\t'.format(len(data)))
 f.write('\n')
 f.write('{:20s}\t'.format('Correct rate'))
-for i in range(0,5):
+for i in range(0, 5):
     if clscnt[i] == 0:
         f.write('{:20f}%\t'.format(0))
     else:
-        f.write('{:20f}%\t'.format(classrtcnt[i]*100.0/clscnt[i]))
-f.write('{:20f}%\t'.format(count * 100.0/len(data)))
+        f.write('{:20f}%\t'.format(classrtcnt[i] * 100.0 / clscnt[i]))
+f.write('{:20f}%\t'.format(count * 100.0 / len(data)))
 f.write('\n\n\n\n')
 f.close()
