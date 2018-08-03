a13654a5cdc1532656fd200f95d41eef1dc4f3b5
unknown <wyc984872769@hotmail.com>
2018-08-03 09:17:22 +0800

    适用于继续之后的任务

--- getpara.py
+++ getpara.py
@@ -7,31 +7,28 @@ class Job:
         self.name = name
         self.classes = []
         self.parameter = ''
-        self.input = ''
-        self.ref = ''
-        self.ini_high = ''
         self.healpix_order = ''
         self.averagepmax = ''
         self.sigma_ang = ''
+        self.offset_range = ''
+        self.offset_step = ''
 
     def getparameter(self):
         try:
-            file = open(self.name + '/' + 'run_it000_optimiser.star')
+            file = open(self.name + '/' + 'run_ct1_it001_optimiser.star')
             self.parameter = file.readlines()[1]
 
             ps = self.parameter.split('--')
 
             for p in ps:
-                if p.find('i ') == 0:
-                    self.input = p.split()[1]
-                if p.find('ref') != -1:
-                    self.ref = p.split()[1]
-                if p.find('ini_high') != -1:
-                    self.ini_high = p.split()[1]
                 if p.find('healpix_order') != -1:
                     self.healpix_order = p.split()[1]
                 if p.find('sigma_ang') != -1:
                     self.sigma_ang = p.split()[1]
+                if p.find('offset_range') != -1:
+                    self.offset_range = p.split()[1]
+                if p.find('offset_step') != -1:
+                    self.offset_step = p.split()[1]
             file.close()
         except IOError, e:
             print(self.name + "is empty")
@@ -67,18 +64,18 @@ names.sort()
 outfile = open('result.txt', 'w')
 
 outfile.write(
-    '{:8s} \t{:40s} \t{:20s} \t{:8s} \t{:13s} \t{:8s} \t{:8s} \t{:8s}\n'.format('jobname', 'input', 'ref', 'ini_high',
-                                                                                'healpix_order', 'sigma_ang',
-                                                                                'averagePmax', 'classes'))
+    '{:8s} \t{:8s} \t{:8s} \t{:8s} \t{:8s} \t{:8s}\n'.format('jobname', 'healpix_order', 'sigma_ang',
+                                                             'offset_range', 'offset_step', 'averagePmax',
+                                                             'classes'))
 
 for name in names:
     job = Job(name)
     job.getparameter()
     job.getmodel()
     outfile.write(
-        '{:8s} \t{:40s} \t{:20s} \t{:8s} \t{:13s} \t{:8s} \t{:8s}\t'.format(name, job.input, job.ref, job.ini_high,
-                                                                            job.healpix_order,
-                                                                            job.sigma_ang, job.averagepmax))
+        '{:8s} \t{:8s} \t{:8s} \t{:8s} \t{:8s}\t'.format(name, job.healpix_order,
+                                                         job.sigma_ang, job.offset_range, job.offset_step,
+                                                         job.averagepmax))
     for cls in job.classes:
         outfile.write('{:8s}\t'.format(cls))
 
---
+++ hebingstar.py
@@ -0,0 +1,181 @@
+# python2.7
+# coding:utf-8
+import os
+import re
+
+import operator
+
+import time
+
+__author__ = 'WangYuchao'
+
+import sys
+import getopt
+
+labels = ['MicrographName', 'CoordinateX', 'CoordinateY', 'ImageName',
+          'DefocusU', 'DefocusV', 'DefocusAngle', 'Voltage',
+          'SphericalAberration', 'AmplitudeContrast',
+          'Magnification', 'DetectorPixelSize', 'CtfFigureOfMerit']
+
+
+# 将star文件读取到内存中
+def readstarfile(filename):
+    f_star = open(filename, 'r')
+    lines = f_star.readlines()
+    headlen = 0
+    headdict = {}
+    for line in lines:
+        headlen += 1
+        if len(line.split()) <= 1:
+            continue
+        elif len(line.split()) > 2:
+            break
+        strlist = line.split()
+        # 将文件头信息存储到字典中
+        headdict.update({strlist[0][4:]: int(strlist[1].strip('#')) - 1})
+    data = []
+    for i in range(headlen - 1, len(lines)):
+        if len(lines[i].split()) < 3:
+            continue
+        data.append(lines[i])
+    f_star.close()
+    return headdict, data
+
+
+# 挑选出star文件中指定类的数据
+def getclassdata(filename, k):
+    headdict, data = readstarfile(filename)
+    newdata = []
+    for line in data:
+        for classnum in k:
+            if int(line.split()[headdict.get('ClassNumber', 0)]) == classnum:
+                newdata.append(line)
+    return headdict, newdata
+
+
+# 存储star文件，支持原数据存储和简洁数据存储
+def savestar(filename, headdict, data, simple=1):
+    file = open(filename, 'w')
+    if simple == 1:
+        headlabel=[]
+        labelitems = []
+        for label in labels:
+            if headdict.get(label) != None:
+                headlabel.append(label)
+                labelitems.append(headdict.get(label, 0))
+        file.write('\ndata_\n\nloop_\n')
+        for i in range(len(headlabel)):
+            file.write('_rln{} #{}\n'.format(headlabel[i],i+1))
+        for item in data:
+            for j in labelitems:
+                file.write('{}\t'.format(item.split()[j]))
+            file.write('\n')
+    else:
+        file.write('\ndata_\n\nloop_\n')
+        head = sorted(headdict.items(), key=operator.itemgetter(1))
+        for name, value in head:
+            file.write('_rln{} #{}\n'.format(name, value + 1))
+        for item in data:
+            file.write(item)
+    file.close()
+
+
+def main():
+    opts, args = getopt.getopt(sys.argv[1:], "dhi:o:s:k:")
+
+    input_file = ""
+    output_file = ""
+    source_file = ""
+    simp = 1
+    k = []
+
+    if len(opts) == 0:
+        usage()
+        sys.exit()
+
+    for op, value in opts:
+        if op == "-i":
+            input_file = value
+        elif op == "-o":
+            output_file = value
+        elif op == "-s":
+            source_file = value
+        elif op == "-k":
+            for i in value.split(','):
+                k.append(int(i))
+        elif op == "-d":
+            simp = 0
+        elif op == "-h":
+            usage()
+            sys.exit()
+
+    if len(input_file) == 0:
+        print("请添加输入文件 使用 -i file_name\n")
+        sys.exit()
+
+    # 如果没有指定输出文件名，自动命名
+    if output_file == "":
+        nowtime = time.strftime("%m%d%H%M%S")
+        if source_file == "":
+            input_path = input_file.split('/')
+            length = len(input_path)
+            if length > 1:
+                output_file = '{}_class'.format(input_path[length - 2])
+            else:
+                output_file = 'class'
+            for item in k:
+                output_file += '_{}'.format(item)
+            output_file += '_{}.star'.format(nowtime)
+        else:
+            output_file = '{}.star'.format(input_file.split('.')[0])
+
+    if source_file == '' and input_file.find('.star') == -1:
+        print("请添加原文件 使用 -s file_name\n")
+        sys.exit()
+
+    if not os.path.exists(input_file):
+        print("{} 文件不存在\n".format(input_file))
+        sys.exit()
+
+    if source_file != "" and not os.path.exists(source_file):
+        print("{} 文件不存在\n".format(source_file))
+        sys.exit()
+
+    if os.path.exists(output_file):
+        print("{} 文件已经存在\n".format(output_file))
+        sys.exit()
+
+    if input_file.find('.star') != -1:
+        if len(k) == 0:
+            print("请输入需要提取的类的序号，使用 －k 0,1")
+            sys.exit()
+        headdict, data = getclassdata(input_file, k)
+        savestar(output_file, headdict, data, simp)
+    else:
+        f_input = open(input_file, 'r')  # 获取需要提取的序号
+        str_index = f_input.readline()
+        index = str_index.split()
+        headdict, data = readstarfile(source_file)
+        newdata = []
+        for item in index:
+            newdata.append(data[int(item)])
+        savestar(output_file, headdict, newdata, simp)
+
+        f_input.close()
+
+    print('output file is {}'.format(output_file))
+
+
+def usage():
+    print("""usage:
+             -i:输入的序号文件名,请使用空格间隔，如输入star文件，则需要参数k，不需要输入源文件
+             -o:输出的star文件名
+             -s:原始star文件名
+             -k:需要匹配的类别，可以输入多个类，‘，’间隔
+             -d:输出star文件中完整的信息
+             -h:帮助
+    """)
+
+
+if __name__ == '__main__':
+    main()
